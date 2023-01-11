ecf7a4cfbb1b9cb71d8a4dad52f19cebe9ea229f
MarjonPhu <marjonphu15@gmail.com>
2023-01-11 14:46:44 +0100

    commit something random voor kacper

---
+++ components/ProfileInfo.js
@@ -0,0 +1,338 @@
+//import react / react-native assets
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  Animated,
+  Platform,
+} from "react-native";
+import React, { useCallback, useState, useRef } from "react";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+import Dashboardpic from "../assets/images/Dashboardpic2.png";
+import pointsIcon from "../assets/images/pointsIcon.png";
+//import expo assets
+
+import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
+
+// create onboarding component
+const ProfileInfo = ({ profilePic, user, navigation }) => {
+  //declare states
+
+  const [dotColor, setDotColor] = useState("#fff");
+  const [lightMode, setLightMode] = useState(true);
+
+  //declare refs
+
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const pressHandler = () => {
+    navigation.navigate("Profile");
+  };
+  const signOut = () => {
+    Securestore.deleteItemAsync("token").then(navigation.navigate("Login"));
+  };
+
+  let coinAmount = 40;
+  const [prefix, suffix] = ["Studies", "Windesheim"];
+  return (
+    <View style={styles.profileView}>
+      <Image style={styles.profilePic} source={profilePic} />
+      <Text style={styles.name}>Gebruiker: {user.name}</Text>
+      <Text style={styles.email}>Email: {user.email}</Text>
+      <Text style={styles.nationality}>nationality: {user.nationality}</Text>
+      <Text style={styles.school}>School: {user.school}</Text>
+      <Text style={styles.study}>Study: {user.school_grade}</Text>
+      {/* <Text style={styles.name}>Geboortedatum: {user.birthdate}</Text> */}
+
+      {/*<View style={styles.profileInfoContainer}>*/}
+      {/* <View style={styles.coin}>
+          <Text style={styles.profileInfo}>{coinAmount}</Text>
+          <Image style={styles.pointsIcon} source={pointsIcon} />
+        </View> */}
+      {/* </View> */}
+    </View>
+  );
+};
+
+// local styles
+const { height, width } = Dimensions.get("screen");
+const styles = StyleSheet.create({
+  ...Platform.select({
+    ios: {
+      coin: {
+        flexDirection: "row",
+        alignContent: "center",
+        width: width / 8,
+        alignItems: "center",
+        justifyContent: "space-between",
+      },
+      profileView: {
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        width: width / 1.18,
+        borderRadius: 25,
+        height: height / 5.5,
+        bottom: getStatusBarHeight() + height / 9,
+
+        justifyContent: "center",
+      },
+      profilePic: {
+        width: width / 4,
+        height: height / 9,
+        top: height * -0.07,
+        left: width / 16,
+      },
+      name: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.04,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      email: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.07,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      nationality: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.1,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      school: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.13,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      study: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.16,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      profileInfoContainer: {
+        width: width / 2,
+        left: width / 14,
+        top: getStatusBarHeight() - height * 0.06,
+      },
+      profileInfo: {
+        color: "#000",
+        fontSize: 14,
+        fontWeight: "bold",
+        position: "relative",
+      },
+      pointsIcon: {
+        width: width / 20,
+        height: height / 40,
+      },
+    },
+    //------------------------------------------------------------android-------------------------------------------------------------------
+    android: {
+      coin: {
+        flexDirection: "row",
+        alignContent: "center",
+        width: width / 8,
+        alignItems: "center",
+        justifyContent: "space-between",
+      },
+      profileView: {
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        width: width / 1.18,
+        borderRadius: 25,
+        height: height / 5.5,
+        bottom: getStatusBarHeight() + height / 1000,
+        justifyContent: "center",
+        alignSelf: "flex-start",
+      },
+      profilePic: {
+        width: width / 4,
+        height: height / 9,
+        top: height * -0.07,
+        left: width / 16,
+      },
+      name: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.04,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      email: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.07,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      nationality: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.1,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      school: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.13,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      study: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.16,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      profileInfoContainer: {
+        width: width / 2,
+        left: width / 14,
+        top: getStatusBarHeight() - height * 0.06,
+      },
+      profileInfo: {
+        color: "#000",
+        fontSize: 14,
+        fontWeight: "bold",
+        position: "relative",
+      },
+      pointsIcon: {
+        width: width / 20,
+        height: height / 40,
+      },
+    },
+    //---------------------------------------------------------------default----------------------------------------------------------------------
+    default: {
+      coin: {
+        flexDirection: "row",
+        alignContent: "center",
+        width: width / 8,
+        alignItems: "center",
+        justifyContent: "space-between",
+      },
+      profileView: {
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        width: width / 1.18,
+        borderRadius: 25,
+        height: height / 5.5,
+        bottom: getStatusBarHeight() + height / 9,
+
+        justifyContent: "center",
+      },
+      profilePic: {
+        width: width / 4,
+        height: height / 9,
+        top: height * -0.07,
+        left: width / 16,
+      },
+      name: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.04,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      email: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.07,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      nationality: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.1,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      school: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.13,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      study: {
+        color: "#107070",
+        fontSize: 16,
+        fontWeight: "bold",
+        top: getStatusBarHeight() + height * 0.16,
+        left: width / 14,
+        position: "absolute",
+
+        //wrap text
+      },
+      profileInfoContainer: {
+        width: width / 2,
+        left: width / 14,
+        top: getStatusBarHeight() - height * 0.06,
+      },
+      profileInfo: {
+        color: "#000",
+        fontSize: 14,
+        fontWeight: "bold",
+        position: "relative",
+      },
+      pointsIcon: {
+        width: width / 20,
+        height: height / 40,
+      },
+    },
+  }),
+});
+
+export default ProfileInfo;
--- screens/Profile.js
+++ screens/Profile.js
@@ -10,11 +10,45 @@ import medLogo from "../assets/images/medLogo.png";
 import Global from "../assets/styles/Global.js";
 import * as Progress from "react-native-progress";
 import * as Securestore from "expo-secure-store";
-import { useState } from "react";
 import backgroundImg from "../assets/images/backgroundImg.png";
 import BottomDrawer from "../components/BottomDrawer";
 import InterestsWidget from "../components/InterestsWidget";
+import ProfileInfo from "../components/ProfileInfo";
+import axios from "axios";
+import profilePic from "../assets/images/profilePic.png";
+import React, { useCallback, useEffect, useState } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+
 export default function Profile({ navigation }) {
+  const [re, setRe] = useState("");
+  const [user, setUser] = useState("");
+  async function getUserData() {
+    try {
+      const response = await axios.get(
+        "https://egabrag.tygoegmond.nl/api/user",
+        {
+          headers: {
+            Authorization:
+              "Bearer " + (await Securestore.getItemAsync("token")),
+            Accept: "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      setUser(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    Securestore.getItemAsync("token").then((token) => {
+      setRe(token);
+    });
+    let data = getUserData();
+  }, []);
+
   const signOut = async () => {
     await Securestore.deleteItemAsync("interests");
     Securestore.deleteItemAsync("token").then(navigation.navigate("Start"));
@@ -23,10 +57,23 @@ export default function Profile({ navigation }) {
     <View style={Global.container}>
       <Image style={styles.imgback2} source={backgroundImg} />
       <Text style={styles.heroText}>Profile</Text>
-      <InterestsWidget navigation={navigation} />
-      <TouchableOpacity style={[Global.button, { top: 500 }]} onPress={signOut}>
-        <Text style={Global.buttonText}>Sign out</Text>
-      </TouchableOpacity>
+      <ScrollView>
+        <View style={styles.container}>
+          <ProfileInfo
+            navigation={navigation}
+            user={user}
+            profilePic={profilePic}
+          />
+          <InterestsWidget navigation={navigation} />
+
+          <TouchableOpacity
+            style={[Global.button, { top: 500 }]}
+            onPress={signOut}
+          >
+            <Text style={Global.buttonText}>Sign out</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
       <BottomDrawer navigation={navigation} />
     </View>
   );
@@ -34,6 +81,14 @@ export default function Profile({ navigation }) {
 const { height, width } = Dimensions.get("screen");
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundImg: backgroundImg,
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: height / 1,
+  },
+
   images: {
     height: height / 5,
     width: width / 1.2,
